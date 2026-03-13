d0f0240f48a27b49974188d7b333eaceb7216446
PlutoRedbean <HD3197388203@outlook.com>
2026-03-13 18:27:19 +0800

    feat: sdram using AXI

--- perip/sdram/sdram_top_axi.v
+++ perip/sdram/sdram_top_axi.v
@@ -39,19 +39,19 @@ module sdram_top_axi(
   output        sdram_we,
   output [12:0] sdram_a,
   output [ 1:0] sdram_ba,
-  output [ 1:0] sdram_dqm,
-  inout  [15:0] sdram_dq
+  output [ 3:0] sdram_dqm,
+  inout  [31:0] sdram_dq
 );
 
   wire sdram_dout_en;
-  wire [15:0] sdram_dout;
-  assign sdram_dq = sdram_dout_en ? sdram_dout : 16'bz;
+  wire [31:0] sdram_dout;
+  assign sdram_dq = sdram_dout_en ? sdram_dout : 32'bz;
   sdram_axi #(
     .SDRAM_MHZ(100),
     .SDRAM_ADDR_W(24),
     .SDRAM_COL_W(9),
     .SDRAM_READ_LATENCY(2)
-  ) u_sdram_axi(
+  ) u_sdram_axi_low(
     .clk_i(clock),
     .rst_i(reset),
     .inport_awvalid_i(in_awvalid),
